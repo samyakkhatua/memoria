dfbd9a32e297012d18dcf2b5ae57645781ddaf6e
samyakkhatua <samyakkhatua@duck.com>
2023-07-05 23:52:08 +0530

    route.js updated for nextjs 13

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,18 @@
-import { authConfig } from "@/lib/auth";
+// import { authConfig } from "@/lib/auth";
 import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      })
+  ],
+  pages: {
+    signIn: '/signin'
+  }
+}
 
 const handler = NextAuth(authConfig);
 
