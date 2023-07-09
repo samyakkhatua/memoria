32eea5129fee66ee80709651a7a6da167937d0fe
samyakkhatua <samyakkhatua@duck.com>
2023-07-09 23:20:12 +0530

    (site) method used for routing

---
+++ app/(site)/login/page.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Login() {
+    return (
+      <>
+      <h1>login page</h1>
+      <Link href="/register">Register</Link>
+      </>
+    )
+  }
