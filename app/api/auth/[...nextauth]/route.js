3883dfc80202d946bf628bb043f576cfc725a9e9
samyakkhatua <samyakkhatua@duck.com>
2023-07-09 23:58:03 +0530

    next-auth providers setup

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,18 +1,66 @@
 // import { authConfig } from "@/lib/auth";
 import NextAuth from "next-auth/next";
+import prisma from "../../../libs/prismadb";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET
-      })
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "John Smith",
+        },
+      },
+      async authorize(credentials) {
+        // check to see if email and password is there
+        if (!credentials.email || !credentials.password) {
+          throw new Error("Please enter an email and password");
+        }
+
+        // check to see if user exists
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        // if no user was found
+        if (!user || !user?.hashedPassword) {
+          throw new Error("No user found");
+        }
+
+        // check to see if password matches
+        const passwordMatch = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+
+        // if password does not match
+        if (!passwordMatch) {
+          throw new Error("Incorrect password");
+        }
+
+        return user;
+      },
+    }),
   ],
+  secret: process.env.SECRET,
   pages: {
-    signIn: '/signin'
-  }
-}
+    signIn: "/signin",
+  },
+};
 
 const handler = NextAuth(authOptions);
 
