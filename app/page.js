58c9eb56787f28177a323539bd132114ff9ed6b2
samyakkhatua <samyakkhatua@duck.com>
2023-07-13 18:56:16 +0530

    use client heading

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { getServerSession  } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import User from "./components/User";
 
 export default async function Home() {
   const session = await getServerSession(authOptions)
@@ -14,6 +15,11 @@ export default async function Home() {
 
     <p>Server Side rendered</p>
     <pre>{JSON.stringify(session)}</pre>
+    <br></br>
+    <br/>
+
+    <p>Client Side rendered</p>
+    <User />
     </>
   )
 }
