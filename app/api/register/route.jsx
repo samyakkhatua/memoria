ae0db8c613cd9d4fecf109d81fb9016952d33967
samyakkhatua <samyakkhatua@duck.com>
2023-07-14 20:26:20 +0530

    prisma client await

--- app/api/register/route.jsx
+++ app/api/register/route.jsx
@@ -3,32 +3,32 @@ import prisma from '../../libs/prismadb'
 import { NextResponse } from 'next/server'
 
 export async function POST(request){
-    const body = await request.json()
+    const body = await request.json();
     const { name, email, password } = body;
 
-    if ( !name || !email || !password) {
-        return new NextResponse('Missing Fields', {status: 400})
+    if(!name || !email || !password) {
+        return new NextResponse('Missing Fields', { status: 400 })
     }
 
-    const exist =  prisma.user.findUnique({
+    const exist = await prisma.user.findUnique({
         where: {
             email
         }
-    })
+    });
 
-    if (exist) {
+    if(exist) {
         throw new Error('Email already exists')
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10)
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = await prisma.user.create({
         data: {
-            name, 
+            name,
             email,
             hashedPassword
         }
-    })
+    });
 
     return NextResponse.json(user)
 }
